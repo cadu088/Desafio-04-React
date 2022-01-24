b35c16a2e4ee27d731e8ff62b058563ea0ff13ec
Carlos Eduardo Rodrigues da Silva <carlos.rodrigues@zema.com>
2022-01-24 13:17:17 -0300

    inicio slug

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
 import styles from './header.module.scss';
+import Logo from '../../other/Logo.svg';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function Header() {
   return (
     <header>
-      <div>Titlw</div>
+      <div className={styles.headerImg}>
+        <img src="../../other/Logo.svg" />
+      </div>
     </header>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { useState } from 'react';
+import { BsCalendarRangeFill } from 'react-icons/bs';
+import { MdPeopleAlt } from 'react-icons/md';
 import { getPrismicClient } from '../services/prismic';
 import Header from '../components/Header';
 import commonStyles from '../styles/common.module.scss';
@@ -25,21 +28,40 @@ interface HomeProps {
 }
 
 export default function Home({ postsPagination }: HomeProps) {
+  function teste() {
+    const a = 15;
+    window.alert('click');
+  }
   return (
     <>
       <Head>
-        <title>Home | Ig.News</title>{' '}
+        <title>Home | Ig.News</title>
       </Head>
       <main>
         <Header />
-
-        <div>
+        <div className={styles.bodyHome}>
           {postsPagination.results.map((p, index) => (
-            <div key={p.uid}>
-              <h1>{p.data}</h1>
-              <h1>{p.data}</h1>
+            <div key={p.uid} className={styles.posts}>
+              <a href={`/post/${p.uid}`}>
+                <h1>{p.data.title}</h1>
+              </a>
+
+              <h3>{p.data.subtitle}</h3>
+              <div className={styles.infoPosts}>
+                <h6>
+                  <BsCalendarRangeFill className={styles.icon} />
+                  {p.first_publication_date}
+                </h6>
+                <h6>
+                  <MdPeopleAlt className={styles.icon} />
+                  {p.data.author}
+                </h6>
+              </div>
             </div>
           ))}
+          <button type="button" onClick={teste}>
+            Carregar mais posts
+          </button>
         </div>
       </main>
     </>
@@ -48,11 +70,34 @@ export default function Home({ postsPagination }: HomeProps) {
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
-  const postsResponse = await prismic.query('Publication', { pageSize: 2 });
+  const postsResponse = await prismic.query('', { pageSize: 3 });
 
-  console.log(postsResponse);
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Date(
+        post.first_publication_date
+      ).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  console.log(posts);
 
   return {
-    props: {},
+    props: {
+      postsPagination: {
+        next_page: 1,
+        results: posts,
+      },
+    },
   };
 };
