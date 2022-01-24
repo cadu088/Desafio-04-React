8c4bd6327274a573b0ac5bfeb75c17f90134596a
Carlos Eduardo Rodrigues da Silva <carlos.rodrigues@zema.com>
2022-01-24 14:59:03 -0300

    .env.local

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,20 +26,44 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post() {
-  // TODO
+export default function Post({ post }: PostProps) {
+  return <h1>post</h1>;
 }
 
-export const getStaticPaths = async () => {
+// export const getStaticPaths = async () => {
+//   // const prismic = getPrismicClient();
+//   // const posts = await prismic.query(TODO);
+//   // // TODO
+// };
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const prismic = getPrismicClient();
-  const posts = await prismic.query(TODO);
+  const { slug } = params;
+  const response = await prismic.getByUID('', String(slug), {});
+  console.log(response);
+  const post = {
+    first_publication_date: response.data.first_publication_date,
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.banner,
+      },
+      author: response.data.author,
+      content: {
+        heading: response.data.content.heading,
+        body: {
+          text: response.data.content.body,
+        },
+      },
+    },
+  };
+  console.log(post);
+  // const post = {
 
-  // TODO
-};
-
-export const getStaticProps = async context => {
-  const prismic = getPrismicClient();
-  const response = await prismic.getByUID(TODO);
-
-  // TODO
+  // }
+  return {
+    props: {
+      post,
+    },
+  };
 };
