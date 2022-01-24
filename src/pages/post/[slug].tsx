08ddb90b9ca3efb7f0fbd640da3b7ed20d0b6fff
Carlos Eduardo Rodrigues da Silva <carlos.rodrigues@zema.com>
2022-01-24 16:49:17 -0300

    🤦‍♂️

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,5 @@
-import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next';
-
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -27,43 +27,51 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps) {
-  return <h1>post</h1>;
+  return <h1>{post.data.author}</h1>;
 }
 
-// export const getStaticPaths = async () => {
-//   // const prismic = getPrismicClient();
-//   // const posts = await prismic.query(TODO);
-//   // // TODO
-// };
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'publication')],
+    {
+      fetch: ['post.title', 'post.subtitle', 'post.author'],
+      pageSize: 100,
+    }
+  );
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const paths = posts.results.map(post => ({ params: { slug: post.uid } }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const prismic = getPrismicClient();
   const { slug } = params;
-  const response = await prismic.getByUID('', String(slug), {});
+  const response = await prismic.getByUID('publication', String(slug), {});
   console.log(response);
+
   const post = {
-    first_publication_date: response.data.first_publication_date,
+    first_publication_date: response.first_publication_date,
     data: {
       title: response.data.title,
       banner: {
-        url: response.data.banner,
+        url: response.data.banner.url,
       },
       author: response.data.author,
-      content: {
-        heading: response.data.content.heading,
-        body: {
-          text: response.data.content.body,
-        },
-      },
+      content: response.data.content[0].map(content => ({
+        heading: content.heading,
+        body: content.body.map(body => ({ text: body.text })),
+      })),
     },
   };
-  console.log(post);
-  // const post = {
-
-  // }
   return {
     props: {
       post,
     },
+    redirect: 60 * 30,
   };
 };
